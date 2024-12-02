c3cd1e501a04f00df78ed1478b5371c03c02571b
Amanda2428 <maymyatthu2428@gmail.com>
2024-12-02 23:35:25 +0630

    hash-error and destory success

--- src/controllers/User.ts
+++ src/controllers/User.ts
@@ -1,3 +1,5 @@
+import bcrypt from "bcrypt";
+import fs from "fs";
 import { UploadedFile } from "express-fileupload";
 import  pool from "../db";
 import { Request, Response } from "express";
@@ -30,12 +32,19 @@ export async function store (req : Request, res : Response)
                 console.log(err)
             });
         }
-        const result = await client.query("INSERT INTO users (name, email, password, profile) VALUES ($1, $2, $3, $4)", [name, email, password, fileName]);
+        const saltRounds = 1;
+        const hashedPassword = await bcrypt.hash(password, saltRounds);
+        console.log("Original Password:", password);
+        console.log("Hashed Password:", hashedPassword);
+        
+        const result = await client.query( "INSERT INTO users (name, email, password, profile) VALUES ($1, $2, $3, $4)",  [name, email, hashedPassword, fileName] );
         client.release();
+
         console.log(result.rows);
         res.json(result.rows);
-    } catch (error) {
-        console.error(error);
+    } catch (e) {
+        console.error("Error in store method:", e);
+        res.status(500).json({ message: "An error occurred"});
     }
 }
 
@@ -57,18 +66,22 @@ export async function getById (req: Request, res : Response){
         console.error(e);
     }
 }
-export async function destroy (req : Request, res : Response) {
-    try{
+export async function destroy(req: Request, res: Response) {
+    try {
         const client = await pool.connect();
-        const id : number = Number(req.params.id);
+        const id: number = Number(req.params.id);
         const result = await client.query("DELETE FROM users WHERE id = $1", [id]);
+
+        if (result.rowCount === 0) {
+            res.status(404).json({ message: "User not found" });
+        } else {
+            res.status(200).json({ message: "User deleted successfully" });
+        }
+
         client.release();
-        console.log(result.rows);
-        res.json(result.rows);
-    }
-    catch(e)
-    {
+    } catch (e) {
         console.error(e);
+        res.status(500).json({ message: "An error occurred" });
     }
 }
 
