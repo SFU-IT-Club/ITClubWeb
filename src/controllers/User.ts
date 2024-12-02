56835f2ae506c3f2b455aff742429ce1d7b4ed3e
Belet-Abaddon <sawmyatmonhnin1420@gmail.com>
2024-12-03 01:35:34 +0630

    search

--- src/controllers/User.ts
+++ src/controllers/User.ts
@@ -1,13 +1,13 @@
 import { UploadedFile } from "express-fileupload";
-import  pool from "../db";
+import pool from "../db";
 import { Request, Response } from "express";
 import path from 'path';
 import IUser from "src/types/IUser";
-export async function getAllUsers (req : Request, res : Response) 
-{
+export async function getAllUsers(req: Request, res: Response) {
     try {
+        const search : string = req.query.search as string;
         const client = await pool.connect();
-        const result = await client.query("SELECT * FROM users");
+        const result = await client.query("SELECT * FROM users WHERE name ILIKE $1",["KPO"]);
         console.log(result.rows);
         client.release();
         res.json(result.rows);
@@ -16,17 +16,16 @@ export async function getAllUsers (req : Request, res : Response)
     }
 }
 
-export async function store (req : Request, res : Response)
-{
+export async function store(req: Request, res: Response) {
     try {
         const client = await pool.connect();
-        const { name, email, password } : IUser = req.body;
-        let fileName : string | null = null;
+        const { name, email, password }: IUser = req.body;
+        let fileName: string | null = null;
 
-        if(req.files?.profile) {
+        if (req.files?.profile) {
             const profile = req.files.profile as UploadedFile;
             fileName = Date.now().toString() + '-' + profile.name;
-            profile.mv(path.join(__dirname, '../../public', fileName), (err)=>{
+            profile.mv(path.join(__dirname, '../../public', fileName), (err) => {
                 console.log(err)
             });
         }
@@ -39,23 +38,34 @@ export async function store (req : Request, res : Response)
     }
 }
 
-export async function update (req : Request, res : Response) {
-    const id : number = Number(req.params.id);
+export async function update(req: Request, res: Response) {
+    const id: number = Number(req.params.id);
 }
 
-export async function getById (req: Request, res : Response){
-    try{
+export async function getById(req: Request, res: Response) {
+    try {
         const client = await pool.connect();
         const id: number = Number(req.params.id);
-        const result = await client.query("SELECT * FROM users WHERE id = $1", [id] );
+        const result = await client.query("SELECT * FROM users WHERE id = $1", [id]);
         client.release();
         console.log(result.rows);
         res.json(result.rows);
     }
-    catch (e)
-    {
+    catch (e) {
         console.error(e);
     }
 }
 
-
+// export async function search(req: Request, res: Response) {
+//     try {
+//         const client = await pool.connect();
+//         const { name }: IUser = req.body;
+//         const result = await client.query("SELECT * FROM users WHERE name ILIKE $1",[`%${name}%`]);
+//         client.release();
+//         console.log(result.rows);
+//         res.json(result.rows);
+//     }
+//     catch (e) {
+//         console.error(e);
+//     }
+// }
