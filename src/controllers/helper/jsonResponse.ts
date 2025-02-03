fb754a51d459b480675fd64dad1b0f6a4905fe72
Meowveloper <meowveloper.dev@gmail.com>
2025-02-04 00:31:15 +0700

    refactor

--- src/controllers/helper/jsonResponse.ts
+++ src/controllers/helper/jsonResponse.ts
@@ -1,38 +1,27 @@
 import { Response } from "express";
 
 // Success Response Function
-export function successResponse(
-  res: Response,
-  data: any,
-  message: string = "Operation successful",
-  token?: string,
-  pagination?: any
-) {
-  const responsePayload = {
-    success: true,
-    data,
-    msg: message,
-    token: token || null,
-    pagination: pagination || null,
-  };
+export function successResponse(res: Response, data: any, message: string = "Operation successful", token?: string, pagination?: any) {
+    const responsePayload = {
+        success: true,
+        data,
+        msg: message,
+        token: token || null,
+        pagination: pagination || null,
+    };
 
-  return res.status(200).json(responsePayload);
+    res.status(200).json(responsePayload);
 }
 
 // Error Response Function
-export function errorResponse(
-  e: Error,
-  status: number,
-  message: string = e.message,
-  res: Response
-) {
-  console.error(e); 
+export function errorResponse(e: Error, status: number, message: string = e.message, res: Response) {
+    console.error(e);
 
-  const responsePayload = {
-    success: false,
-    error: message,
-    status,
-  };
+    const responsePayload = {
+        success: false,
+        error: message,
+        status,
+    };
 
-  return res.status(status).json(responsePayload);
+    res.status(status).json(responsePayload);
 }
