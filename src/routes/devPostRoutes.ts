d8b83c006a5de8330ce54df1e6c24a6efecce8e0
Amanda2428 <maymyatthu2428@gmail.com>
2025-02-06 01:06:48 +0630

    DevPost function

--- src/routes/devPostRoutes.ts
+++ src/routes/devPostRoutes.ts
@@ -1,8 +1,13 @@
 import express from "express";
+import { storeDevPost, destroyDevPost, updateDevPost, getByDevPostsID } from "../controllers/DevPost"; 
 
 const devPostRoutes = express.Router();
 
 
-devPostRoutes.get('/');
+devPostRoutes.post('/store', storeDevPost);
+devPostRoutes.delete('/:id', destroyDevPost);
+devPostRoutes.put('/:id',updateDevPost);
+devPostRoutes.get('/:id', getByDevPostsID);
+
 
 export default devPostRoutes;
