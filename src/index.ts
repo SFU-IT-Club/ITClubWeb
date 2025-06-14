f80213a36e4a295eb073dbadc8e1fb710246f9d7
Kyaw Paing Oo <82670447+Kyawpaingoo@users.noreply.github.com>
2025-06-14 22:01:11 +0700

    blah

--- src/index.ts
+++ src/index.ts
@@ -88,7 +88,6 @@ app.get("/users", (req: Request, res: Response) => {
 app.get("/develop-post", (req: Request, res: Response) => {
     res.render('dev-form', {
         title: 'dev-form',
-
         error: req.query.errorResponse,
     })
 });
